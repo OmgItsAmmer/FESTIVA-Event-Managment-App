2a68c007b19abaa5b60c86520818888a0018f703
usama-codes <uayaz.bese23seecs@seecs.edu.pk>
2024-05-04 11:24:04 +0500

    Login and authentication done

--- app/src/main/java/com/example/nustapp/login/Login.java
+++ app/src/main/java/com/example/nustapp/login/Login.java
@@ -10,6 +10,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.nustapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 //import com.google.firebase.auth.FirebaseAuth;
 //import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 
@@ -19,8 +21,8 @@ public class Login extends AppCompatActivity {
 
     EditText emailAddress, password;
 
-    //    //FirebaseAuth auth = FirebaseAuth.getInstance();
-//
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+
     @SuppressLint("MissingInflatedId")
 
     @Override
@@ -28,42 +30,46 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-//        emailAddress = findViewById(R.id.emailAddress);
-//        password = findViewById(R.id.password);
-//        Button RegisterButton = findViewById(R.id.register);
-//        Button LogInButton = findViewById(R.id.loginButton);
-//
-//        LogInButton.setOnClickListener(v -> {
-//            String user = emailAddress.getText().toString();
-//            String pass = password.getText().toString();
-//                login(user, pass);
-//        });
-//
-//        RegisterButton.setOnClickListener(v -> {
-//            Intent registerintent = new Intent(getApplicationContext(), Register.class);
-//            startActivity(registerintent);
-//        });
-//    }
+        emailAddress = findViewById(R.id.emailAddress);
+        password = findViewById(R.id.password);
+        Button RegisterButton = findViewById(R.id.register);
+        Button LogInButton = findViewById(R.id.loginButton);
 
-        //method for login
-//    public void login(String email, String password) {
-//        if (email.isEmpty() || password.isEmpty()) {
-//            Toast.makeText(Login.this, "Empty Credentials", Toast.LENGTH_SHORT).show();
-//        } else if (password.length() < 6) {
-//            Toast.makeText(Login.this, "Password must be at least 6 characters", Toast.LENGTH_SHORT).show();
-//        } else {
-//            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
-//                if (task.isSuccessful()) {
-//                    startActivity(new Intent(getApplicationContext(), com.example.login.GetUserInfo.class));
-//                } else {
-//                    if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
-//                        Toast.makeText(Login.this, "Invalid credentials. Please try again.", Toast.LENGTH_SHORT).show();
-//                    } else {
-//                        Toast.makeText(Login.this, "Login Failed: " + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
-//                    }
-//                }
-//            });
+//        if(auth.getCurrentUser() != null) {
+//            startActivity(new Intent(getApplicationContext(), com.example.nustapp.Activity.MainActivity.class));
 //        }
-//    }
+
+        LogInButton.setOnClickListener(v -> {
+            String user = emailAddress.getText().toString();
+            String pass = password.getText().toString();
+                login(user, pass);
+        });
+
+        RegisterButton.setOnClickListener(v -> startActivity(new Intent(this, Register.class)));
+    }
+
+    //method for login
+    public void login(String email, String password) {
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(Login.this, "Empty Credentials", Toast.LENGTH_SHORT).show();
+        }
+        else if (password.length() < 6) {
+            Toast.makeText(Login.this, "Password must be at least 6 characters", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    startActivity(new Intent(getApplicationContext(), com.example.nustapp.Activity.MainActivity.class));
+                }
+                else {
+                    if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                        Toast.makeText(Login.this, "Invalid credentials. Please try again.", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(Login.this, "Login Failed: " + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
--- app/src/main/java/com/example/nustapp/login/Register.java
+++ app/src/main/java/com/example/nustapp/login/Register.java
@@ -5,6 +5,7 @@ import static android.widget.Toast.makeText;
 import static com.example.nustapp.R.*;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,62 +13,83 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.nustapp.R;
-//import com.google.firebase.auth.FirebaseAuth;
-//import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
+    Map<String, Object> users = GetUserInfo.users;
     EditText signUpEmailAddress, signUpPassword, confirmPassword;
     Button register;
-    //FirebaseDatabase rootNode;
-//    FirebaseAuth auth;
+     FirebaseAuth auth;
+     FirebaseFirestore db = GetUserInfo.db;
     String user;
     String pass;
     String repass;
 
 
-//    @SuppressLint("MissingInflatedId")
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(layout.activity_register);
-//        //Assigning ids to variables
-//        auth = FirebaseAuth.getInstance();
-//        signUpEmailAddress = findViewById(id.emailAddress);
-//        signUpPassword = findViewById(id.password);
-//        confirmPassword = findViewById(id.confirmPassword);
-//        register = findViewById(id.RegisterButton);
-//
-//        register.setOnClickListener(v -> {
-//            //Get user input
-//            user = signUpEmailAddress.getText().toString();
-//            pass = signUpPassword.getText().toString();
-//            repass = confirmPassword.getText().toString();
-//
-//            signIn(user, pass, repass);
-//        });
-//    }
-//
-//    public void signIn(String user, String pass, String repass) {
-//        if (user.isEmpty() || pass.isEmpty() || repass.isEmpty()) {
-//            Toast.makeText(Register.this, "Empty Credentials", Toast.LENGTH_SHORT).show();
-//        } else if (pass.length() < 6) {
-//            Toast.makeText(Register.this, "Password must be at least 6 characters", Toast.LENGTH_SHORT).show();
-//        } else if (!pass.equals(repass)) {
-//            Toast.makeText(Register.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
-//        }
-//        else {
-//            auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(task -> {
-//                if (task.isSuccessful()) {
-//                    makeText(Register.this, "User Created", Toast.LENGTH_SHORT).show();
-//                } else {
-//                    if (task.getException() instanceof FirebaseAuthUserCollisionException) {
-//                        makeText(Register.this, "User Already Exists", Toast.LENGTH_SHORT).show();
-//                    } else {
-//                        makeText(Register.this, "User Not Created", Toast.LENGTH_SHORT).show();
-//                    }
-//                }
-//            });
-//        }
-//    }
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_register);
+
+        auth = FirebaseAuth.getInstance();
+        signUpEmailAddress = findViewById(id.emailAddress);
+        signUpPassword = findViewById(id.password);
+        confirmPassword = findViewById(id.confirmPassword);
+        register = findViewById(id.RegisterButton);
+
+        register.setOnClickListener(v -> {
+            user = signUpEmailAddress.getText().toString();
+            pass = signUpPassword.getText().toString();
+            repass = confirmPassword.getText().toString();
+
+            signIn(user, pass, repass);
+            Intent intent = new Intent(Register.this, GetUserInfo.class);
+            intent.putExtra("email", user);
+            users.put("email", user);
+            db.collection("users").document(user);
+            startActivity(intent);
+        });
+    }
+
+    public void signIn(String user, String pass, String repass) {
+        if (user.isEmpty() || pass.isEmpty() || repass.isEmpty()) {
+            Toast.makeText(Register.this, "Empty Credentials", Toast.LENGTH_SHORT).show();
+        }
+        else if (pass.length() < 6) {
+            Toast.makeText(Register.this, "Password must be at least 6 characters", Toast.LENGTH_SHORT).show();
+        }
+        else if (!pass.equals(repass)) {
+            Toast.makeText(Register.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    FirebaseUser firebaseUser = auth.getCurrentUser();
+                    if (firebaseUser != null) {
+                        firebaseUser.sendEmailVerification().addOnCompleteListener(task1 -> {
+                            if (task1.isSuccessful()) {
+                                makeText(Register.this, "Verification email sent", Toast.LENGTH_SHORT).show();
+                            } else {
+                                makeText(Register.this, "Failed to send verification email", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }
+                    else {
+                    if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                        makeText(Register.this, "User Already Exists", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        makeText(Register.this, "User Not Created", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
 }
